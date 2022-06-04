e732ead83f52b6807cce94ad20a2795c5872d6d5
Davi Milhome <davimmilhome@gmail.com>
2022-06-04 20:24:56 -0300

    Nova modificação

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pandas as pd, pandas_datareader as wb, numpy as np, seaborn as sns, matplotlib.pyplot as plt
 
-btg = wb.DataReader('BPAC11.SA', data_source = 'yahoo', start = '2018-01-01')
+ticket_escolhido = input('Escolha um ticket: ')
 
-print(btg.columns)
+def actual_price(ticket):
+    ticket_df = wb.DataReader(f'{ticket}', data_source = 'yahoo', start = '2018-01-01')
+    print(f'Fechamento:\n {ticket_df["Close"].tail(1)}')
 
-btg['High'].plot()
-#plt.plot(btg['High'])
-plt.show()
+
+# btg = wb.DataReader('BPAC11.SA', data_source = 'yahoo', start = '2018-01-01')
+#
+# print(btg.columns)
+#
+# btg['High'].plot()
+# #plt.plot(btg['High'])
+# plt.show()
+
+if __name__ == '__main__':
+    actual_price(ticket_escolhido)
+
+
 
