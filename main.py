dbf6744cbc5cbe05481cdbf8047f917093661ff4
Davi Milhome <davimmilhome@gmail.com>
2022-06-04 23:33:30 -0300

    V1_introdução_médias

--- main.py
+++ main.py
@@ -4,10 +4,49 @@ ticket_escolhido = input('Escolha um ticket: ')
 
 def actual_price(ticket):
     ticket_df = wb.DataReader(f'{ticket}', data_source = 'yahoo', start = '2018-01-01')
-    print(f'Fechamento:\n {ticket_df["Close"].tail(1)}')
+
+   # ticket_df_close = ticket_df["Close"]
+    #ticket_df_close["Index_date"] = ticket_df.index()
 
 
-# btg = wb.DataReader('BPAC11.SA', data_source = 'yahoo', start = '2018-01-01')
+
+    #Adiciona a coluna date e substitui o index por números inteiros
+    ticket_df.reset_index(inplace = True)
+    #ticket_df_close.reset_index(inplace=True)
+
+    #Fechamento - Preço e data de fechamento
+    v_actual_date = ticket_df["Date"].iloc[-1].strftime('%d/%m/%y')
+    v_actual_price = round(ticket_df["Close"].iloc[-1],2)
+
+    #Médias
+    df_mean_6m = ticket_df["Close"].tail(180)
+    v_mean_6m = round(df_mean_6m.mean(), 2)
+
+    df_mean_12m = ticket_df["Close"].tail(360)
+    v_mean_12m = round(df_mean_12m.mean(), 2)
+
+    df_mean_36m = ticket_df["Close"].tail(1080)
+    v_mean_36m = round(df_mean_36m.mean(), 2)
+
+    df_mean_60m = ticket_df["Close"].tail(1800)
+    v_mean_60m = round(df_mean_60m.mean(), 2)
+
+    list_mean = [v_mean_60m,v_mean_36m,v_mean_12m,v_mean_6m]
+    df_mean = pd.DataFrame(list_mean)
+
+    #Saída
+    print(f'Último fechamento: {v_actual_date,v_actual_price}')
+    print(f'Média de fechamento dos últimos 5 anos - 3 anos - 1 ano - 6 meses\n{list_mean}')
+
+    #plt.plot(df_mean)
+    x = ['média_5_anos','média_3_anos','média_1_ano','média_6_meses']
+    plt.plot(x,list_mean)
+    plt.title(f'Movimento da média do ativo: {ticket}')
+    # plt.bar(df_mean,'1')
+    plt.show()
+
+
+#btg = wb.DataReader('BPAC11.SA', data_source = 'yahoo', start = '2018-01-01')
 #
 # print(btg.columns)
 #
