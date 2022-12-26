543aacb4e4010c65f46c29fac2167e026f29954c
Davi Milhome <davimmilhome@gmail.com>
2022-12-26 03:18:41 -0300

    TESTE FUNCIONALIDADE PHP

--- INIT_FILES/stocks.php
+++ INIT_FILES/stocks.php
@@ -1,7 +1,17 @@
 <?php
+ini_set('display_errors',0);
+ini_set('log_errors',1);
+echo whoami;
 // Print a greeting if the form was submitted
 if ($_POST['stock']) {
 print "Ação escolhida: ";print $_POST['stock'];
+$stock_name = $_POST['stock'];
+//chmod("/", 777);
+print"\n start opening open";
+$FileHandle = fopen("1.txt", w); print"try to open";
+$FileHandle = fopen("1.txt", w) or die("can't open file");
+fclose($FileHandle);
+print"\n fim do bloco" ;
 } else {
 // Otherwise, print the form
 print <<<_HTML_
