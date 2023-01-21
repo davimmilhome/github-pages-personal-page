1ed95c30921d885fefb867674b84afc1e76e88ea
Davi Milhome <davimmilhome@gmail.com>
2023-01-21 16:18:15 -0300

    feat: trabalhando no seletor de ações

--- INIT_FILES/stocks.php
+++ INIT_FILES/stocks.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="../style/style.css">
-        <h1>ola</h1>
+        <h1>Analisador de ações</h1>
     <head>
 </html>
 
@@ -15,30 +15,35 @@ define('style/style.css', 'template/css/'); //define CSS path
  -->
 <?php
 ini_set('display_errors',1);
-// init_set('display_startup_erros', 1);
-ini_set('log_errors',1);
+//init_set('display_startup_erros', 1);
+// ini_set('log_errors',1);
 // Print a greeting if the form was submitted
-if ($_POST['stock']) {
-print "Ação escolhida: ";print $_POST['stock'];
-$stock_name = $_POST['stock'];
+if ($_POST['stock-picked']) {
+print "Ação escolhida: ";print $_POST['stock-picked'];
+$stock_name = $_POST['stock-picked'];
 //chmod("/", 777);
 print"<br> start opening";
-$FileHandle = fopen("$stock_name .txt", w); print"\n try to open";
+$FileHandle = fopen("stocks/stock-picked/stock-picked.env","w");
+$stock_env = "STOCK-PICKED"."=\"$stock_name\"";
+fwrite($FileHandle, $stock_env);
 fclose($FileHandle);
 print"\n fim do bloco" ;
 } else {
 // Otherwise, print the form
 print <<<_HTML_
 <form method="post" action="$_SERVER[PHP_SELF]">
-    <fieldset name="stock-picked">
-     <legend>Escolha uma ação da B3</legend>
-     <input type="text"
-      placeholder="Ex: bbas3">
-     <button type="submit"  class="button medium-btn" name="stock-picked"></button>
+    <fieldset name="stock-picked-fds">
+        <legend>Escolha uma ação da B3</legend>
+        <input type="text"
+        placeholder="Ex: bbas3"
+        name="stock-picked">
+        <button 
+        type="submit"
+        name="stock-picked-btn">Confirmar
+        </button>
     </fieldset>
 </form>
 _HTML_;
-
 }
 ?>
 
