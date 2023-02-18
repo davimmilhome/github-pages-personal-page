c4c51c766a815ba583a81d88e0e654fefc3b9364
Davi Milhome <davimmilhome@gmail.com>
2023-02-17 21:31:15 -0300

    feat: adicionando modais aos botões

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,33 @@
-console.log('Hello World!')
+/*Modal construct genérico*/
+const modalConstruct = document.getElementById("modalConstruct")
 
-function construction() {
-    alert("Esse botão está em construção ;)");
-}
+/*
+Função exibit modal
+
+object -  quando setado, pega o objeto do dom , no caso,
+um modal e aplica o display block para ele aparecer.
+*/
+function functionAppear(self) {
+    self.style.display = "block"
+};
+
+/*Botão de fechar*/
+const closeButton = document.getElementById("close-button");
+closeButton.addEventListener("click", function() {
+    modalContact.style.display = "none";
+});
+
+const aboutButton = document.getElementById("aboutButton")
+
+const portfButton = document.getElementById("portfButton")
+portfButton.addEventListener("click", functionAppear(modalConstruct))
+
+
+
+/*Modal contato*/
+const contactButton = document.getElementById("contactButton")
+const modalContact = document.getElementById("modalContact");
+
+contactButton.addEventListener("click", function() {
+    modalContact.style.display = "block";
+});
