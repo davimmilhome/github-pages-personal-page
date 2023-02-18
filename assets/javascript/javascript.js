b117c14bf73e964e96e37b4d68bb242272857f7a
Davi Milhome <davimmilhome@gmail.com>
2023-02-18 07:54:28 -0300

    fix: corrigindo botãoes dos modais

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,33 +1,35 @@
-/*Modal construct genérico*/
-const modalConstruct = document.getElementById("modalConstruct")
-
 /*
 Função exibit modal
 
 object -  quando setado, pega o objeto do dom , no caso,
 um modal e aplica o display block para ele aparecer.
 */
+
 function functionAppear(self) {
     self.style.display = "block"
 };
 
+function functionClose(self) {
+    self.style.display = "none"
+};
+
 /*Botão de fechar*/
 const closeButton = document.getElementById("close-button");
-closeButton.addEventListener("click", function() {
-    modalContact.style.display = "none";
-});
 
+/*Modal construct genérico*/
+const modalConstruct = document.getElementById("modalConstruct")
+
+
+/*Modal about*/
 const aboutButton = document.getElementById("aboutButton")
-
-const portfButton = document.getElementById("portfButton")
-portfButton.addEventListener("click", functionAppear(modalConstruct))
+aboutButton.addEventListener("click", functionAppear.bind(null, modalConstruct))
+closeButton.addEventListener("click", functionClose.bind(null, modalConstruct))
 
 
 
 /*Modal contato*/
 const contactButton = document.getElementById("contactButton")
-const modalContact = document.getElementById("modalContact");
+const modalContact = document.getElementById("modalContact")
+contactButton.addEventListener("click", functionAppear.bind(null,modalContact))
+closeButton.addEventListener("click", functionClose.bind(null,modalContact))
 
-contactButton.addEventListener("click", function() {
-    modalContact.style.display = "block";
-});
